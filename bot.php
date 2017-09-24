7960540bbcb4f7790b4dab926020090a26b761c5
jirapasjipipop <32241707+jirapasjipipop@users.noreply.github.com>
2017-09-24 23:44:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == "สวัสดี") {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => "สวัสดีครับ",
+				'type' => 'text',
 				'text' => $text
 			];
 
