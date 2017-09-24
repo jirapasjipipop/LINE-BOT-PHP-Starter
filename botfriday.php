50662c4113fd4907d4136e00a115e7b7d47bddf0
jirapasjipipop <32241707+jirapasjipipop@users.noreply.github.com>
2017-09-24 23:34:43 +0700

    Create botfriday.php

---
+++ botfriday.php
@@ -0,0 +1,38 @@
+<?php
+ 
+$strAccessToken = "BVfLp1yOUeUZpcqoRbdApEW6hqHvCCNLSAWPdRkYW1otsToobQ2qTvcyte5t0G8wXTtUJuZF71f0orZZuZe+halI1AXHiwZaxZbTCDR5xSCMPVvlRwSIOsuesPAwEjWAlREQ7aR9XPEiFV1/tNgdBAdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดีครับ"];
+}else if($arrJson['events'][0]['message']['text'] == "คุณชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "เราชื่อ Friday";
+
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
